b9d27bb3a30032adaf33940121e2c411adedd15d
jiwon317 <chlghqlt12@naver.com>
2023-09-28 23:33:56 +0900

    StringExam

---
+++ src/javastudy/StringExam.java
@@ -0,0 +1,12 @@
+package javastudy;
+
+public class StringExam {
+
+	public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = "hello";
+		
+		String str3 = new String("hello");
+		String str4 = new String("hello");
+	}
+}
