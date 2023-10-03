c056bbe83036b6c816deb872be8d770b712e95ec
jiwon317 <chlghqlt12@naver.com>
2023-10-03 19:59:25 +0900

    Method

---
+++ src/javastudy/ClassExam.java
@@ -0,0 +1,9 @@
+package javastudy;
+
+public class ClassExam {
+	public static void main(String[]args) {
+		MyClass2 mc = new MyClass2();
+		System.out.println(mc.myMethod(10));
+		System.out.println(mc.myMethod(20));
+	}
+}
---
+++ src/javastudy/MyClass2.java
@@ -0,0 +1,11 @@
+package javastudy;
+
+public class MyClass2 {
+	public int myMethod(int value) {
+		return value;
+	}
+}
+	
+
+
+
