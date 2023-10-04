3f683f461a538ac510b71831535ba42d2b1fe0f5
jiwon317 <chlghqlt12@naver.com>
2023-10-04 23:23:20 +0900

    StringMethodExam

---
+++ src/javastudy/StringMethodExam.java
@@ -0,0 +1,10 @@
+package javastudy;
+
+public class StringMethodExam {
+
+	public static void main(String[] args) {
+//		String str = new String("hello");
+		String str = "hello";
+		System.out.println(str.length());
+	}
+}
