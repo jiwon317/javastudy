3da2354805d51a4cbaa79c16666078635bf50b63
jiwon317 <chlghqlt12@naver.com>
2023-10-04 23:19:08 +0900

    ReferenceTypeExam

---
+++ src/javastudy/Car1.java
@@ -0,0 +1,7 @@
+package javastudy;
+
+public class Car1 {
+	public void run() {
+		System.out.println("차가 달립니다.");
+	}
+}
---
+++ src/javastudy/CarExam1.java
@@ -0,0 +1,8 @@
+package javastudy;
+
+public class CarExam1 {
+	public static void main(String[] args) {
+		Car1 car = new Car1();
+		car.run();
+	}
+}
